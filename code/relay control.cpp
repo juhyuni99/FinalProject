341e890c0a8c65283738e2913c7251b3741a4d8f
euning <52948715+eunee99@users.noreply.github.com>
2022-06-20 18:23:09 +0900

    Final update

--- code/relay control.cpp
+++ code/relay control.cpp
@@ -1,23 +1,55 @@
 #include <Arduino.h>
 #include <IBMIOTF8266.h>
+#include <Wire.h>
+#include <SSD1306.h>
+#include <DHTesp.h>
 
-// USER CODE EXAMPLE : Publish Interval. The periodic update is normally recommended.
-// And this can be a good example for the user code addition
-String user_html = "";
-char *ssid_pfix = (char *)"IoTSecure";
+SSD1306 display(0x3c, 4, 5, GEOMETRY_128_32);
 
 unsigned long lastPublishMillis = -pubInterval;
 
 const int RELAY = 15;
+
+String user_html = "";
+char *ssid_pfix = (char *)"IoTdata";
+
+#define DHTPIN 14
+DHTesp dht;
+float temp_f;
+unsigned long lastDHTReadMillis = 0;
+#define interval 2000
+
+void gettemperature()
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastDHTReadMillis >= interval)
+  {
+    lastDHTReadMillis = currentMillis;
+
+    temp_f = dht.getTemperature(); // Read temperature as Fahrenheit
+  }
+}
 
 void publishData()
 {
   StaticJsonDocument<512> root;
   JsonObject data = root.createNestedObject("d");
 
-  // USER CODE EXAMPLE : command handling
+  char dht_buf[10];
+
+  gettemperature();
+
+  display.setColor(BLACK);
+  display.fillRect(80, 16, 100, 33);
+  display.setColor(WHITE);
+
+  sprintf(dht_buf, "%2.1f", temp_f);
+  display.drawString(80, 16, dht_buf);
+  data["temperature"] = dht_buf;
+
+  display.display();
+
   data["mode"] = digitalRead(RELAY) ? "secure" : "daily";
-  // USER CODE EXAMPLE : command handling
 
   serializeJson(root, msgBuffer);
   client.publish(publishTopic, msgBuffer);
@@ -27,9 +59,7 @@ void handleUserCommand(JsonDocument *root)
 {
   JsonObject d = (*root)["d"];
 
-  // USER CODE EXAMPLE : status/change update
-  // code if any of device status changes to notify the change
-  if (d.containsKey("mode"))
+    if (d.containsKey("mode"))
   {
     if (strcmp(d["mode"], "secure"))
     {
@@ -41,7 +71,6 @@ void handleUserCommand(JsonDocument *root)
     }
     lastPublishMillis = -pubInterval;
   }
-  // USER CODE EXAMPLE
 }
 
 void message(char *topic, byte *payload, unsigned int payloadLength)
@@ -57,11 +86,12 @@ void message(char *topic, byte *payload, unsigned int payloadLength)
   }
 
   handleIOTCommand(topic, &root);
+
   if (!strcmp(updateTopic, topic))
   {
   }
   else if (!strncmp(commandTopic, topic, 10))
-  { // strcmp return 0 if both string matches
+  {
     handleUserCommand(&root);
   }
 }
@@ -69,13 +99,17 @@ void message(char *topic, byte *payload, unsigned int payloadLength)
 void setup()
 {
   Serial.begin(115200);
-  // USER CODE EXAMPLE : meta data update
+
+  dht.setup(DHTPIN, DHTesp::DHT22);
+
   pinMode(RELAY, OUTPUT);
-  // USER CODE EXAMPLE
+
+  display.init();
+  display.flipScreenVertically();
+  display.drawString(30, 5, "IoT Thermostat");
+  display.drawString(30, 16, "Current");
 
   initDevice();
-  // If not configured it'll be configured and rebooted in the initDevice(),
-  // If configured, initDevice will set the proper setting to cfg variable
 
   WiFi.mode(WIFI_STA);
   WiFi.begin((const char *)cfg["ssid"], (const char *)cfg["w_pw"]);
@@ -100,13 +134,13 @@ void setup()
 
 void loop()
 {
+
   if (!client.connected())
   {
     iot_connect();
   }
 
   client.loop();
-
   if ((pubInterval != 0) && (millis() - lastPublishMillis > pubInterval))
   {
     publishData();
